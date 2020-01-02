bdc1e077d4b7c48c6a24839560c0b3398359a9ec
Ignacio Bonafonte <47814950+nacho-undefinedlabs@users.noreply.github.com>
2020-01-02 10:27:14 +0100

    Implementation of the BAtch Span Processor class (#12)

--- Sources/OpenTelemetrySdk/Trace/Export/BatchSpanProcessor.swift
+++ Sources/OpenTelemetrySdk/Trace/Export/BatchSpanProcessor.swift
@@ -23,9 +23,101 @@ import Foundation
 /// If the queue gets half full a preemptive notification is sent to the worker thread that
 /// exports the spans to wake up and start a new export cycle.
 /// This batchSpanProcessor can cause high contention in a very high traffic service.
-public struct BatchSpanProcessor /*: SpanProcessor */ {
-    private let workerThreadName = "BatchSpanProcessor_WorkerThread"
+public struct BatchSpanProcessor: SpanProcessor {
     var sampled: Bool
+    fileprivate var worker: BatchWorker
 
-    // TODO: Missing all
+    init(spanExporter: SpanExporter, sampled: Bool = true, scheduleDelay: TimeInterval = 5, maxQueueSize: Int = 2048, maxExportBatchSize: Int = 512) {
+        worker = BatchWorker(spanExporter: spanExporter,
+                             scheduleDelay: scheduleDelay,
+                             maxQueueSize: maxQueueSize,
+                             maxExportBatchSize: maxExportBatchSize)
+        worker.start()
+        self.sampled = sampled
+    }
+
+    public func onStart(span: ReadableSpan) {
+    }
+
+    public func onEnd(span: ReadableSpan) {
+        if sampled && !span.context.traceFlags.sampled {
+            return
+        }
+        worker.addSpan(span: span)
+    }
+
+    public func shutdown() {
+        worker.cancel()
+        worker.flush()
+    }
+}
+
+/// BatchWorker is a thread that batches multiple spans and calls the registered SpanExporter to export
+/// the data.
+/// The list of batched data is protected by a NSCondition which ensures full concurrency.
+private class BatchWorker: Thread {
+    let spanExporter: SpanExporter
+    let scheduleDelay: TimeInterval
+    let maxQueueSize: Int
+    let maxExportBatchSize: Int
+    let halfMaxQueueSize: Int
+    private let cond = NSCondition()
+    var spanList = [ReadableSpan]()
+
+    init(spanExporter: SpanExporter, scheduleDelay: TimeInterval, maxQueueSize: Int, maxExportBatchSize: Int) {
+        self.spanExporter = spanExporter
+        self.scheduleDelay = scheduleDelay
+        self.maxQueueSize = maxQueueSize
+        halfMaxQueueSize = maxQueueSize >> 1
+        self.maxExportBatchSize = maxExportBatchSize
+    }
+
+    func addSpan(span: ReadableSpan) {
+        cond.lock()
+        defer { cond.unlock() }
+
+        if spanList.count == maxQueueSize {
+            // TODO: Record a counter for dropped spans.
+            return
+        }
+        // TODO: Record a gauge for referenced spans.
+        spanList.append(span)
+        // Notify the worker thread that at half of the queue is available. It will take
+        // time anyway for the thread to wake up.
+        if spanList.count >= halfMaxQueueSize {
+            cond.broadcast()
+        }
+    }
+
+    override func main() {
+        repeat {
+            var spansCopy: [ReadableSpan]
+            cond.lock()
+            if spanList.count < maxExportBatchSize {
+                repeat {
+                    cond.wait(until: Date().addingTimeInterval(scheduleDelay))
+                } while spanList.count == 0
+            }
+            spansCopy = spanList
+            spanList.removeAll()
+            cond.unlock()
+            exportBatches(spanList: spansCopy)
+        } while true
+    }
+
+    func flush() {
+        var spansCopy: [ReadableSpan]
+        cond.lock()
+        spansCopy = spanList
+        spanList.removeAll()
+        cond.unlock()
+        // Execute the batch export outside the synchronized to not block all producers.
+        exportBatches(spanList: spansCopy)
+    }
+
+    private func exportBatches(spanList: [ReadableSpan]) {
+        stride(from: 0, to: spanList.endIndex, by: maxExportBatchSize).forEach {
+            spanExporter.export(spans: spanList[$0 ..< min($0 + maxExportBatchSize, spanList.count)].map { $0.toSpanData() })
+        }
+    }
 }
